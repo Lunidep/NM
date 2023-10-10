534bf2c50c2bf23ee312f71c551cd27f7afcc19c
Lunidep <ilya151102@yandex.ru>
2023-10-10 11:05:02 +0300

    lab4 varChange

--- lab4/lab4_2.py
+++ lab4/lab4_2.py
@@ -8,22 +8,19 @@ from lab4.lab4_1 import runge_kutta_method, runge_romberg_method, mae
 
 
 def f(x, y, z):
-    return (x * z - y) / (x * (x - 1))
+    return (y - (x - 3) * z) / (x ** 2 - 1)
 
 
 def g(x, y, z):
     return z
 
 
-# Functions for finite difference method
-# y'' + p_fd(x)y' + q_fd(x)y = f_fd(x)
-
 def p_fd(x):
-    return -1 / (x - 1)
+    return (x - 3) / (x ** 2 - 1)
 
 
 def q_fd(x):
-    return 1 / (x * (x - 1))
+    return -1 / (x ** 2 - 1)
 
 
 def f_fd(x):
@@ -31,7 +28,7 @@ def f_fd(x):
 
 
 def exact_solution(x):
-    return 2 + x + 2 * x * math.log(abs(x))
+    return x - 3 + (1 / (x + 1))
 
 
 def get_n(n_prev, n, ans_prev, ans, b, delta, gamma, y1):
@@ -76,7 +73,6 @@ def finite_difference_method(p, q, f, y0, yn, alpha, beta, delta, gamma, interva
     x = np.arange(a, b + h, h)
     n = len(x)
 
-    # Creating tridiagonal matrix
     for i in range(n):
         if i == 0:
             rows.append(alpha * h - beta)
@@ -116,69 +112,51 @@ def finite_difference_method(p, q, f, y0, yn, alpha, beta, delta, gamma, interva
     return x, y
 
 
-def print_task():
-    task = """
-Given problem:
-x * (x - 1) * y'' - x * y' + y  = 0
-y'(1) = 3
-y(3) - 3 * y'(3) = -4
-
-We will convert this to system of equations with order = 1:
-y' = g(x, y, z) = z
-z' = f(x, y, z) = (x * y' - y) / (x * (x - 1))
-z(1) = 3
-y(3) - 3 * z(3) = -4
-"""
-    print(task)
-
-
 if __name__ == '__main__':
-    interval = (1.000001, 3)  # x in [1; 3]
-    y0 = 3
-    y1 = -4
+    interval = (0.000001, 1)
+    y0 = 0
+    y1 = -0.75
     h = 0.1
     eps = 0.001
-    alpha, beta, delta, gamma = 0, 1, 1, -3
-
-    print_task()
+    alpha, beta, delta, gamma = 0, 1, 1, 1
 
     x_shooting, y_shooting, iters_shooting = shooting_method(f, g, alpha, beta, delta, gamma, y0, y1, interval, h, eps)
-    plt.plot(x_shooting, y_shooting, label=f'shooting method, step={h}')
+    plt.plot(x_shooting, y_shooting, label=f'Метод стрельбы, шаг={h}')
     x_shooting2, y_shooting2, iters_shooting2 = shooting_method(f, g, alpha, beta, delta, gamma, y0, y1, interval, h / 2, eps)
-    plt.plot(x_shooting2, y_shooting2, label=f'shooting method, step={h / 2}')
+    plt.plot(x_shooting2, y_shooting2, label=f'Метод стрельбы, шаг={h / 2}')
 
     x_fd, y_fd = finite_difference_method(p_fd, q_fd, f_fd, y0, y1, alpha, beta, delta, gamma, interval, h)
-    plt.plot(x_fd, y_fd, label=f'finite difference method, step={h}')
+    plt.plot(x_fd, y_fd, label=f'finite difference method, шаг={h}')
     x_fd2, y_fd2 = finite_difference_method(p_fd, q_fd, f_fd, y0, y1, alpha, beta, delta, gamma, interval, h / 2)
-    plt.plot(x_fd2, y_fd2, label=f'finite difference method, step={h / 2}')
+    plt.plot(x_fd2, y_fd2, label=f'finite difference method, шаг={h / 2}')
 
     x_exact = [i for i in np.arange(interval[0], interval[1] + h, h)]
     x_exact2 = [i for i in np.arange(interval[0], interval[1] + h / 2, h / 2)]
     y_exact = [exact_solution(x_i) for x_i in x_exact]
     y_exact2 = [exact_solution(x_i) for x_i in x_exact2]
-    plt.plot(x_exact, y_exact, label='exact solution')
+    plt.plot(x_exact, y_exact, label='Точное решение')
 
-    print('Iterations')
-    print(f'Step = {h}')
-    print('shooting:', iters_shooting)
-    print(f'Step = {h / 2}')
-    print('shooting:', iters_shooting2)
+    print('Итерации')
+    print(f'шаг = {h}')
+    print('Метод стрельбы: ', iters_shooting)
+    print(f'шаг = {h / 2}')
+    print('Метод стрельбы: ', iters_shooting2)
     print()
 
-    print('Mean absolute errors')
-    print(f'Step = {h}')
-    print('finite difference:', mae(y_fd, y_exact))
-    print(f'Step = {h / 2}')
-    print('finite difference:', mae(y_fd2, y_exact2))
-    print(f'Step = {h}')
-    print('shooting:', mae(y_shooting, y_exact))
-    print(f'Step = {h / 2}')
-    print('shooting:', mae(y_shooting2, y_exact2))
+    print('Значения абсолютных ошибок')
+    print(f'шаг = {h}')
+    print('Конечно-разностный метод решения: ', mae(y_fd, y_exact))
+    print(f'шаг = {h / 2}')
+    print('Конечно-разностный метод решения: ', mae(y_fd2, y_exact2))
+    print(f'шаг = {h}')
+    print('Метод стрельбы: ', mae(y_shooting, y_exact))
+    print(f'шаг = {h / 2}')
+    print('Метод стрельбы: ', mae(y_shooting2, y_exact2))
     print()
 
-    print('Runge-Romberg accuracy')
-    print('shooting:', runge_romberg_method(h, h / 2, y_shooting, y_shooting2, 1))
-    print('finite difference:', runge_romberg_method(h, h / 2, y_fd, y_fd2, 4))
+    print('Оценка погрешности методом Рунге-Румберга')
+    print('Метод стрельбы: ', runge_romberg_method(h, h / 2, y_shooting, y_shooting2, 1))
+    print('Конечно-разностный метод решения: ', runge_romberg_method(h, h / 2, y_fd, y_fd2, 4))
 
     plt.legend()
     plt.show()
