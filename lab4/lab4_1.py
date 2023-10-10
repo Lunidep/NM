5bb51ae649fed86ccda41a57c4d19c55778bce10
Ilya <ilya151102@yandex.ru>
2023-10-10 11:07:16 +0300

    Add files via upload

--- lab4/lab4_1.py
+++ lab4/lab4_1.py
@@ -1,9 +1,11 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def f(x, y, z):
-    return (2 * x * z - 2 * y) / (x**2 + 1)
+    return ((x + 1) * z - y) / x
 
 
 def g(x, y, z):
@@ -11,19 +13,10 @@ def g(x, y, z):
 
 
 def exact_solution(x):
-    return x - x**2 + 1
+    return x + 1 + (math.e ** x)
 
 
 def euler_method(f, g, y0, z0, interval, h):
-    """
-    Solve cauchy problem:
-    y' = g(x, y, z)
-    z' = f(x, y, z)
-    y(0) = y0
-    z(0) = z0
-    where x in interval, h - step using euler method
-    Returns solution - tabular function y=f(x) as two arrays: x, y
-    """
     l, r = interval
     x = [i for i in np.arange(l, r + h, h)]
     y = [y0]
@@ -35,16 +28,6 @@ def euler_method(f, g, y0, z0, interval, h):
 
 
 def runge_kutta_method(f, g, y0, z0, interval, h, return_z=False, exclude_num=None):
-    """
-    Solve cauchy problem:
-    y' = g(x, y, z)
-    z' = f(x, y, z)
-    y(0) = y0
-    z(0) = z0
-    where x in interval, h - step using runge-kutta method
-    Returns solution - tabular function y=f(x) as two arrays: x, y
-    If return_z is true, function will also return coefficients for z
-    """
     l, r = interval
     x = [i for i in np.arange(l, r + h, h)]
     if exclude_num:
@@ -72,15 +55,6 @@ def runge_kutta_method(f, g, y0, z0, interval, h, return_z=False, exclude_num=No
 
 
 def adams_method(f, g, y0, z0, interval, h):
-    """
-    Solve cauchy problem:
-    y' = g(x, y, z)
-    z' = f(x, y, z)
-    y(0) = y0
-    z(0) = z0
-    where x in interval, h - step using adams method
-    Returns solution - tabular function y=f(x) as two arrays: x, y
-    """
     x_runge, y_runge, z_runge = runge_kutta_method(f, g, y0, z0, interval, h, return_z=True)
     x = x_runge
     y = y_runge[:4]
@@ -99,11 +73,7 @@ def adams_method(f, g, y0, z0, interval, h):
     return x, y
 
 
-def runge_romberg_method(h1, h2, y1, y2, p):
-    """
-    Find more accuracy of solution using previous calculations.
-    Works if h1 == 2 * h2
-    """
+def runge_romberg_method(h1, h2, y1, y2, p): # p - порядок точности
     assert h1 == h2 * 2
     norm = 0
     for i in range(len(y1)):
@@ -112,10 +82,6 @@ def runge_romberg_method(h1, h2, y1, y2, p):
 
 
 def mae(y1, y2):
-    """
-    Find mean absolute error between y1 and y2
-    MAE = 1/n sum(|y1-y2|)
-    """
     assert len(y1) == len(y2)
     res = 0
     for i in range(len(y1)):
@@ -123,64 +89,48 @@ def mae(y1, y2):
     return res / len(y1)
 
 
-def print_task():
-    task = """
-Given Cauchy problem:
-(x ^ 2 + 1) * y'' - 2 * x * y' + 2 *  y  = 0
-y(0) = 1
-y'(0) = 1
-We will convert this to system of equations with order = 1:
-y' = g(x, y, z) = z
-z' = f(x, y, z) = (2 * x * y' - 2 *  y) / (x ^ 2 + 1)
-y(0) = 1
-z(0) = 1
-"""
-    print(task)
-
-
 if __name__ == '__main__':
-    y0 = 1  # y(0) = 1
-    dy0 = 1  # y'(0) = 1
-    interval = (0, 1)  # x in [0; 1]
+    y0 = 2 + math.e
+    dy0 = 1 + math.e
+    interval = (1, 2)
     h = 0.1
-    print_task()
 
     x_euler, y_euler = euler_method(f, g, y0, dy0, interval, h)
-    plt.plot(x_euler, y_euler, label=f'euler method, step={h}')
+    plt.plot(x_euler, y_euler, label=f'Метод Эйлера, шаг={h}')
     x_euler2, y_euler2 = euler_method(f, g, y0, dy0, interval, h/2)
-    plt.plot(x_euler2, y_euler2, label=f'euler method, step={h/2}')
+    plt.plot(x_euler2, y_euler2, label=f'Метод Эйлера, шаг={h/2}')
 
     x_runge, y_runge = runge_kutta_method(f, g, y0, dy0, interval, h)
-    plt.plot(x_runge, y_runge, label=f'runge kutta method, step={h}')
+    plt.plot(x_runge, y_runge, label=f'Метод Рунге-Кутта, шаг={h}')
     x_runge2, y_runge2 = runge_kutta_method(f, g, y0, dy0, interval, h/2)
-    plt.plot(x_runge2, y_runge2, label=f'runge kutta method, step={h/2}')
+    plt.plot(x_runge2, y_runge2, label=f'Метод Рунге-Кутта, шаг={h/2}')
 
     x_adams, y_adams = adams_method(f, g, y0, dy0, interval, h)
-    plt.plot(x_adams, y_adams, label=f'adams method, step={h}')
+    plt.plot(x_adams, y_adams, label=f'Метод Адамса, шаг={h}')
     x_adams2, y_adams2 = adams_method(f, g, y0, dy0, interval, h/2)
-    plt.plot(x_adams2, y_adams2, label=f'adams method, step={h/2}')
+    plt.plot(x_adams2, y_adams2, label=f'Метод Адамса, шаг={h/2}')
 
     x_exact = [i for i in np.arange(interval[0], interval[1] + h, h)]
     x_exact2 = [i for i in np.arange(interval[0], interval[1] + h/2, h/2)]
     y_exact = [exact_solution(x_i) for x_i in x_exact]
     y_exact2 = [exact_solution(x_i) for x_i in x_exact2]
-    plt.plot(x_exact, y_exact, label='exact solution')
+    plt.plot(x_exact, y_exact, label='Точное решение')
 
-    print('Mean absolute errors')
-    print(f'Step = {h}')
-    print('euler:', mae(y_euler, y_exact))
-    print('runge-kutta:', mae(y_runge, y_exact))
-    print('adams:', mae(y_adams, y_exact))
-    print(f'Step = {h/2}')
-    print('euler:', mae(y_euler2, y_exact2))
-    print('runge-kutta:', mae(y_runge2, y_exact2))
-    print('adams:', mae(y_adams2, y_exact2))
+    print('Значения абсолютных ошибок')
+    print(f'шаг = {h}')
+    print('Метод Эйлера: ', mae(y_euler, y_exact))
+    print('Метод Рунге-Кутта: ', mae(y_runge, y_exact))
+    print('Метод Адамса: ', mae(y_adams, y_exact))
+    print(f'шаг = {h/2}')
+    print('Метод Эйлера: ', mae(y_euler2, y_exact2))
+    print('Метод Рунге-Кутта: ', mae(y_runge2, y_exact2))
+    print('Метод Адамса: ', mae(y_adams2, y_exact2))
     print()
 
-    print('Runge-Romberg accuracy')
-    print('euler:', runge_romberg_method(h, h / 2, y_euler, y_euler2, 1))
-    print('runge-kutta:', runge_romberg_method(h, h / 2, y_runge, y_runge2, 4))
-    print('adams:', runge_romberg_method(h, h / 2, y_adams, y_adams2, 4))
+    print('Оценка погрешности методом Рунге-Румберга')
+    print('Метод Эйлера: ', runge_romberg_method(h, h / 2, y_euler, y_euler2, 1))
+    print('Метод Рунге-Кутта: ', runge_romberg_method(h, h / 2, y_runge, y_runge2, 4))
+    print('Метод Адамса: ', runge_romberg_method(h, h / 2, y_adams, y_adams2, 4))
 
     plt.legend()
     plt.show()
